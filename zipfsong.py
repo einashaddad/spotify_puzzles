2e429858f5d1c4682e7df3fb5f484896d07831cd
Einas Haddad <einas@haddad>
2013-05-14 23:12:20 -0400

    Account for EOF in testfile

--- zipfsong.py
+++ zipfsong.py
@@ -13,29 +13,30 @@ def zipfsongs(file_content):
     l_songs = file_content.split('\n')
     output_count = get_output_count(l_songs)
     song_frequencies = [song.split(' ') for song in l_songs[1:]]
-    quality = calc_quality(song_frequencies)
-    return [quality[i][0] for i in xrange(output_count)]
+    song_ranks = song_qualities(song_frequencies)
+    return [song_ranks[i][0] for i in xrange(output_count)]
 
-def calc_quality(song_frequencies):
+def song_qualities(song_frequencies):
     '''
     Calculates the quality according to zipf score and actual frequency,
     returns a list of tuples sorted by quality first and position
     in album second. 
     '''
     first_song_freq = float(song_frequencies[0][0])
-    quality = []    
+    song_ranks = []    
     for i, song in enumerate(song_frequencies):
         song_name = song[1]
         song_frequency = float(song[0])
         track_num = i + 1
         zipf = first_song_freq / track_num
-        quality.append((song_name, song_frequency/zipf))
-    return sorted(quality, key=lambda tup: tup[1], reverse=True)
+        song_ranks.append((song_name, song_frequency/zipf))
+    return sorted(song_ranks, key=lambda tup: tup[1], reverse=True)
 
 def get_output_count(l_songs):
     stats = l_songs[0].split(' ')
     return int(stats[1])
 
 if __name__ == '__main__':
-    print zipfsongs(sys.stdin.read())
-
+    lines = sys.stdin.read()
+    ranks = zipfsongs(lines.rstrip())
+    print '\n'.join(ranks)
